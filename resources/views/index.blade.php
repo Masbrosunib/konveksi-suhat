ebd1053994f082debb390944c34aef430a98552a
ahmadhafidz04 <ahmad.hafidz04@outlook.com>
2024-05-30 08:05:44 +0700

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+  <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+
+  <title>Konveksi Suhat</title>
+
+  <!-- slider stylesheet -->
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+
+  <!-- Custom styles for this template -->
+  <link href="css/style.css" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="css/responsive.css" rel="stylesheet" />
+</head>
+
+<body>
+
+  <div id="trailer">
+
+  </div>
+
+  <!-- Navigation Bar starts -->
+  <header class="header_section">
+    <nav class="navbar navbar-expand-lg custom_nav-container">
+      <div class="container px-4">
+          <a class="navbar-brand" href="index.html">
+            <img id="logo-konveksi" src="images/favicon.png" alt="Konveksi Suhat Logo">
+            <span> Konveksi Suhat</span>
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class=""></span>
+          </button>
+
+          <div class="collapse navbar-collapse innerpage_navbar" id="navbarSupportedContent">
+              <ul class="navbar-nav ms-auto mb-2 mb-lg-0 small fw-bolder">
+                  <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
+                  <li class="nav-item"><a class="nav-link" href="shop.html">Products</a></li>
+                  <li class="nav-item"><a class="nav-link" href="orders.html">My Orders</a></li>
+                  <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+              </ul>
+          </div>
+      </div>
+    </nav>
+  </header>
+  <!-- Navigation bar ends -->
+  <section class="slider_section">
+    <div class="slider_container">
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <div class="container-fluid">
+              <div class="row">
+                <div class="col-md-7">
+                  <div class="detail-box">
+                    <h1>
+                      Konveksi <br>
+                      Suhat
+                    </h1>
+                    <p>
+                      Sequi perspiciatis nulla reiciendis, rem, tenetur impedit, eveniet non necessitatibus error distinctio mollitia suscipit. Nostrum fugit doloribus consequatur distinctio esse, possimus maiores aliquid repellat beatae cum, perspiciatis enim, accusantium perferendis.
+                    </p>
+                    <a href="">
+                      Contact Us
+                    </a>
+                  </div>
+                </div>
+                <div class="col-md-5 ">
+                  <div class="img-box">
+                    <img src="images/varsity.png" alt="" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="shop_section layout_padding">
+    <div class="container">
+      <div class="heading_container heading_center">
+        <h2>
+          Services Available
+        </h2>
+      </div>
+      <div class="row">
+        <div class="product-container">
+          <div class="box">
+            <a href="">
+              <div class="img-box">
+                <img src="images/varsity.png" alt="">
+              </div>
+              <div class="detail-box">
+                <h6>Varsity</h6>
+                <h6><span>Rp39k</span></h6>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="product-container">
+          <div class="box">
+            <a href="">
+              <div class="img-box">
+                <img src="images/varsity.png" alt="">
+              </div>
+              <div class="detail-box">
+                <h6>Varsity</h6>
+                <h6><span>Rp39k</span></h6>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="product-container">
+          <div class="box">
+            <a href="">
+              <div class="img-box">
+                <img src="images/varsity.png" alt="">
+              </div>
+              <div class="detail-box">
+                <h6>Varsity</h6>
+                <h6><span>Rp39k</span></h6>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="product-container">
+          <div class="box">
+            <a href="">
+              <div class="img-box">
+                <img src="images/varsity.png" alt="">
+              </div>
+              <div class="detail-box">
+                <h6>Varsity</h6>
+                <h6><span>Rp39k</span></h6>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="btn-box">
+        <a href="">
+          View All Products
+        </a>
+      </div>
+    </div>
+  </section>
+
+  <script src="index.js"></script>
+  <script src="js/jquery-3.4.1.min.js"></script>
+  <script src="js/bootstrap.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js">
+  </script>
+  <script src="js/custom.js"></script>
+
+</body>
+
+</html>
