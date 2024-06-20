59b8317d6e6007db95d4ee49baa2d0f02c90fedb
ahmadhafidz04 <ahmad.hafidz04@outlook.com>
2024-06-20 13:43:55 +0700

    profile progress

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -1,8 +1,11 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Storage;
+
 
 class ProfileController extends Controller
 {
@@ -19,30 +22,14 @@ class ProfileController extends Controller
             'phone' => 'required|string|max:15',
             'email' => 'required|email|max:255',
             'address' => 'nullable|string|max:255',
-            'subdistrict' => 'nullable|string|max:255',
-            'city' => 'nullable|string|max:255',
-            'province' => 'nullable|string|max:255',
-            'postcode' => 'nullable|string|max:10',
-            'profile_image' => 'nullable|image|max:2048',
         ]);
 
         $user = Auth::user();
+        $user = User::find($user->id);
         $user->name = $request->input('name');
         $user->phone = $request->input('phone');
         $user->email = $request->input('email');
         $user->address = $request->input('address');
-        $user->subdistrict = $request->input('subdistrict');
-        $user->city = $request->input('city');
-        $user->province = $request->input('province');
-        $user->postcode = $request->input('postcode');
-
-        if ($request->hasFile('profile_image')) {
-            if ($user->profile_image) {
-                Storage::delete($user->profile_image);
-            }
-            $path = $request->file('profile_image')->store('profile_images');
-            $user->profile_image = $path;
-        }
 
         $user->save();
 
